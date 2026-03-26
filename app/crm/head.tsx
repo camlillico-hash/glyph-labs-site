6a82c6fc9a963683d2c7de53d494d0f38cbb6178
Cam <camlillico@gmail.com>
2026-03-26 13:56:04 -0400

    Serve CRM favicon from public asset

--- app/crm/head.tsx
+++ app/crm/head.tsx
@@ -2,8 +2,8 @@ export default function Head() {
   return (
     <>
       <title>Glyph Labs | CRM</title>
-      <link rel="icon" href="/crm/icon.png?v=2" sizes="any" />
-      <link rel="shortcut icon" href="/crm/icon.png?v=2" />
+      <link rel="icon" href="/crm-icon.png?v=bcf53ab" sizes="any" />
+      <link rel="shortcut icon" href="/crm-icon.png?v=bcf53ab" />
     </>
   );
 }
